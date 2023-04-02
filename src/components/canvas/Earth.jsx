acbcc5b9946dd89cde440aacb07b2a182df8b610
thanhtan0501 <bttthanh0501@gmail.com>
2023-04-03 06:26:12 +0700

    update

--- src/components/canvas/Earth.jsx
+++ src/components/canvas/Earth.jsx
@@ -5,7 +5,7 @@ import React, { Suspense } from "react";
 import CanvasLoader from "../Loader";
 
 const Earth = () => {
-    const earth = useGLTF("public/planet/scene.gltf");
+    const earth = useGLTF("./planet/scene.gltf");
     return (
         <primitive
             object={earth.scene}
