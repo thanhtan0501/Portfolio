eeb51c3ccf579d3868c5670ffb053c8cde8c7d0a
thanhtan0501 <bttthanh0501@gmail.com>
2024-02-28 16:47:03 +0700

    fix richtext error

--- src/app/_components/ProjectDetail/RelatedProjects.tsx
+++ src/app/_components/ProjectDetail/RelatedProjects.tsx
@@ -10,20 +10,20 @@ const RelatedProjects = ({ data }: { data: Project[] }) => {
       {data && data.length > 0 && (
         <>
           <h2
-            id="related-projects"
+            id="RELATED%20PROJECTS"
             tabIndex={-1}
             className="scroll-mt-16 flex gap-x-2 items-center font-bold uppercase text-fluid-1 mb-2"
           >
             <span>Related Projects</span>
             <a
-              href="#technologies"
+              href="#RELATED%20PROJECTS"
               title="permalink"
               className="w-11 h-11 text-text-1 grid place-items-center hover:bg-surface-4 rounded-md hover:text-brand-stroke transition-all"
             >
               <LinkIcon className="w-5 h-5" />
             </a>
           </h2>
-          <div className=" h-max grid  grid-cols-1 sm:grid-cols-2 2xl:grid-cols-3 py-4 gap-4">
+          <div className=" h-max grid  grid-cols-1 sm:grid-cols-2 2xl:grid-cols-3 pb-4 gap-4">
             {data.map((item, i) => (
               <div key={i} className="h-full w-full rounded-lg">
                 <RelatedProjectCard data={item} />
