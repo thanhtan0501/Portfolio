0401aaccd5879e50ed55d25624df2791864d54ec
thanhtan0501 <bttthanh0501@gmail.com>
2024-03-01 16:42:43 +0700

    Fix display error

--- src/app/_components/Nav/index.tsx
+++ src/app/_components/Nav/index.tsx
@@ -4,7 +4,7 @@ import Link from 'next/link'
 
 const Nav = () => {
   return (
-    <nav className="flex justify-between bg-surface-alpha sticky top-0 left-0 z-10 p-2 navbar-blur px-4">
+    <nav className="flex justify-between bg-surface-alpha sticky top-0 left-0 z-100 p-2 navbar-blur px-4">
       <Link
         className="w-11 h-11 text-text-1 grid place-items-center hover:bg-surface-3 rounded-md transition-all"
         href="/"
